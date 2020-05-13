9a285f9ac75e1d51e19ee50bb86b5f999dc66d1a
Niklas Sturm <>
2020-05-14 01:01:40 +0200

    Implemented pulling data from facebook part of API.

---
+++ receiver/receiver/facebook.py
@@ -0,0 +1,51 @@
+import aiohttp
+
+from receiver import utils
+
+
+async def get_data():
+    endpoints = [
+        "counter_ads.json",
+        "fb_spiderplot.json",
+        "fb_sentiment.json",
+        "fb_posts.json",
+        "fb_likes.json",
+        "fb_shares.json",
+    ]
+
+    base_url = "https://political-dashboard.com/json_files/"
+
+    async with aiohttp.ClientSession() as session:
+        data = await utils.get_data_from_endpoints(session, endpoints, base_url, fetch)
+    return data
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        data = await response.json()
+
+    filename = utils.get_filename_from_url(url)
+    if filename == "counter_ads":
+        data_type = "ads"
+        transformed_data = await transform_counter(data, data_type)
+    else:
+        filename = filename[3:]
+        if filename == "spiderplot":
+            data_type = "reactions"
+        else:
+            data_type = filename
+        transformed_data = await transform(data, data_type)
+    return transformed_data
+
+
+async def transform_counter(data, data_type):
+    transformed_data = {
+        "data_type": data_type,
+        "count": data["chart"][0]["1"]["count"],
+    }
+    return transformed_data
+
+
+async def transform(data, data_type):
+    transformed_data = {"data_type": data_type, "items": data["chart"]}
+    return transformed_data
