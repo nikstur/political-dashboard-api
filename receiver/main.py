012358a6673a07265b5d4ebd7d1bd8f97b49092e
Niklas Sturm <>
2020-05-14 01:02:23 +0200

    Added function calls for facebook data.

--- receiver/main.py
+++ receiver/main.py
@@ -7,18 +7,23 @@ from urllib.parse import urlparse
 import aiohttp
 from pymongo import MongoClient
 
-from receiver import twitter, utils
+from receiver import twitter, facebook, utils
 
-# client = MongoClient(host="172.17.0.2")
-client = MongoClient(host="db")
+client = MongoClient(host="172.17.0.2")
+# client = MongoClient(host="db")
 db = client["database"]
 db.drop_collection("twitter")
 twitter_col = db["twitter"]
+db.drop_collection("facebook")
+facebook_col = db["facebook"]
 
 
 async def main():
-    twitter_results = await twitter.get_twitter()
+    twitter_results = await twitter.get_data()
     twitter_col.insert_many(twitter_results)
+
+    facebook_results = await facebook.get_data()
+    facebook_col.insert_many(facebook_results)
 
 
 if __name__ == "__main__":
