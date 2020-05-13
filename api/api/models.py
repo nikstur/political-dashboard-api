8219a77ad37a31477df0450b666cd7f3cf6193a9
Niklas Sturm <>
2020-05-14 01:02:45 +0200

    Streamlined endpoints and added those for facebook data.

--- api/api/models.py
+++ api/api/models.py
@@ -2,6 +2,8 @@ from typing import List, Union
 
 from pydantic import BaseModel
 
+# Twitter
+
 
 class TwitterURLResponseBody(BaseModel):
     text: str
@@ -23,3 +25,49 @@ class TwitterHashtagResponse(BaseModel):
     data_type: str = "hashtags"
     party: str = None
     items: List[TwitterHashtagResponseBody]
+
+
+# Facebook
+
+
+class SimpleFacebookResponseBody(BaseModel):
+    text: str
+    count: int
+    biased: int
+    hours: int
+
+
+class SimpleFacebookResponse(BaseModel):
+    data_type: str
+    items: List[SimpleFacebookResponseBody]
+
+
+class FacebookReactionsBody(BaseModel):
+    text: str
+    AfD: float
+    CDU: float
+    CSU: float
+    FDP: float
+    Gruenen: float
+    Linke: float
+    SPD: float
+
+
+class FacebookReactionsReponse(BaseModel):
+    data_type: str = "reactions"
+    items: List[FacebookReactionsBody]
+
+
+class FacebookSentimentBody(BaseModel):
+    text: str
+    posts: float
+
+
+class FacebookSentimentResponse(BaseModel):
+    data_type: str = "sentiment"
+    items: List[FacebookSentimentBody]
+
+
+class FacebookAdsResponse(BaseModel):
+    data_type: str = "ads"
+    count: int
--- api/main.py
+++ api/main.py
@@ -9,17 +9,15 @@ from api import models, utils
 
 app = FastAPI()
 
-# client = MongoClient(host="172.17.0.2")
-client = MongoClient(host="db")
+client = MongoClient(host="172.17.0.2")
+# client = MongoClient(host="db")
 db = client["database"]
 twitter_col = db["twitter"]
 facebook_col = db["facebook"]
 media_col = db["media"]
 
 
-@app.get("/")
-async def root():
-    return {"message": "Not yet implemented"}
+# Twitter
 
 
 @app.get(
@@ -37,7 +35,7 @@ async def twitter():
     response_model=List[models.TwitterHashtagResponse],
     response_model_exclude_unset=True,
 )
-async def hashtags(
+async def twitter_hashtags(
     party: str = Query(None, description="Abbreviated name of party", min_length=3)
 ):
     if party == None:
@@ -48,14 +46,55 @@ async def hashtags(
         )
 
 
-@app.get("/twitter/urls", response_model=models.TwitterURLResponse)
-async def urls():
-    return {"message": "Not yet implemented"}
+# Facebook
 
 
-@app.get("/facebook")
+@app.get(
+    "/facebook",
+    response_model=List[
+        Union[
+            models.SimpleFacebookResponse,
+            models.FacebookReactionsReponse,
+            models.FacebookSentimentResponse,
+            models.FacebookAdsResponse,
+        ]
+    ],
+)
 async def facebook():
-    return {"message": "Not yet implemented"}
+    return utils.get_cleaned_docs(facebook_col)
+
+
+@app.get("/facebook/posts", response_model=List[models.SimpleFacebookResponse])
+async def facebook_posts():
+    return utils.get_cleaned_docs(facebook_col, {"data_type": "posts"})
+
+
+@app.get("/facebook/shares", response_model=List[models.SimpleFacebookResponse])
+async def facebook_shares():
+    return utils.get_cleaned_docs(facebook_col, {"data_type": "shares"})
+
+
+@app.get("/facebook/likes", response_model=List[models.SimpleFacebookResponse])
+async def facebook_likes():
+    return utils.get_cleaned_docs(facebook_col, {"data_type": "likes"})
+
+
+@app.get("/facebook/reactions", response_model=List[models.FacebookReactionsReponse])
+async def facebook_reactions():
+    return utils.get_cleaned_docs(facebook_col, {"data_type": "reactions"})
+
+
+@app.get("/facebook/sentiment", response_model=List[models.FacebookSentimentResponse])
+async def facebook_sentiment():
+    return utils.get_cleaned_docs(facebook_col, {"data_type": "sentiment"})
+
+
+@app.get("/facebook/ads", response_model=List[models.FacebookAdsResponse])
+async def facebook_ads():
+    return utils.get_cleaned_docs(facebook_col, {"data_type": "ads"})
+
+
+# Media
 
 
 @app.get("/media")
