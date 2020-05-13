1124a31635aa13105afb1d94585912da2545fa27
Niklas Sturm <>
2020-05-14 01:04:56 +0200

    Restored db connection string for docker.

--- receiver/main.py
+++ receiver/main.py
@@ -9,8 +9,8 @@ from pymongo import MongoClient
 
 from receiver import twitter, facebook, utils
 
-client = MongoClient(host="172.17.0.2")
-# client = MongoClient(host="db")
+# client = MongoClient(host="172.17.0.2")
+client = MongoClient(host="db")
 db = client["database"]
 db.drop_collection("twitter")
 twitter_col = db["twitter"]
