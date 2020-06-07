117e62fb965407b244755842694ecbd3aa1703c2
Niklas Sturm <>
2020-06-07 13:23:29 +0200

    Add timestamps to each downloaded document (#15)

--- receiver/receiver/fetch/preprocess/media.py
+++ receiver/receiver/fetch/preprocess/media.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from typing import Dict
 
 
 def transform(data: Dict, data_type: str) -> Dict:
-    transformed_data = {"data_type": data_type}
+    transformed_data = {"data_type": data_type, "time": datetime.now()}
     if data_type == "topics":
         transformed_data["items"] = data["children"]
     else:
