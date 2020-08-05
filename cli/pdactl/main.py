48104b54d517588529f5bbb3e1a9340e7b80b666
nikstur <nikstur@outlook.com>
2020-08-05 18:24:38 +0200

    Improved CLI printing, data validation and error handling

--- cli/pdactl/main.py
+++ cli/pdactl/main.py
@@ -1,13 +1,13 @@
 from configparser import ConfigParser
-from typing import Dict, List, Optional
+from typing import Optional
 
 import typer
 
+from .commands import key
 from .config import ensure_config, write_config
-from .fetching import get, get_full_url, post
-from .printing import print_header, print_line
 
 app = typer.Typer()
+app.add_typer(key.app, name="key")
 
 
 @app.command()
@@ -16,16 +16,14 @@ def setup(url: str = typer.Option(..., "-u", "--url", prompt=True)):
     if url:
         config("core.url", url)
     typer.echo(
-        'Now export your api key as an evironment variable by typing the following into your shell: \n export PDA_API_KEY="<YOUR_API_KEY>"'
+        'Now export your api key as an evironment variable: export PDA_API_KEY="<YOUR_API_KEY>"'
     )
 
 
 @app.command()
 def config(
     key: str = typer.Argument(..., help="Config option to get or set."),
-    value: Optional[str] = typer.Argument(
-        False, help="Value to set the config option to."
-    ),
+    value: Optional[str] = typer.Argument(False, help="Value of config option."),
 ):
     """Get and set configuration options"""
     top_key, sub_key = key.split(".")
@@ -43,45 +41,6 @@ def config(
                 f"No configuration option available under this key: {e}", err=True
             )
     write_config(config)
-
-
-@app.command()
-def add(
-    can_create_token: bool = typer.Option(
-        False,
-        "-c",
-        "--can-create-token",
-        help="Gives key the power to create new keys.",
-    ),
-    amount: int = typer.Argument(1, help="Amount of keys to create."),
-):
-    """Generate new key(s) and store their hash in the database"""
-    url: str = get_full_url("/administration/add_key")
-    request_body: Dict = {"can_create_tokens": can_create_token}
-    new_keys: List[Dict] = [post(url, request_body) for i in range(amount)]
-    print_header(add_key=True)
-    for new_key in new_keys:
-        print_line(new_key, add_key=True)
-
-
-@app.command()
-def remove(identifier: int = typer.Argument(..., help="Identifier of key to remove.")):
-    """Remove key(s) from the database"""
-    url: str = get_full_url("/administration/remove_key")
-    request_body: Dict = {"identifier": identifier}
-    doc: Dict = post(url, request_body)
-    print_header()
-    print_line(doc)
-
-
-@app.command()
-def ls():
-    """List all keys in the database"""
-    url: str = get_full_url("/administration/keys")
-    keys: List[Dict] = get(url)
-    print_header()
-    for key in keys:
-        print_line(key)
 
 
 if __name__ == "__main__":
--- cli/pdactl/printing.py
+++ cli/pdactl/printing.py
@@ -1,35 +1,62 @@
 from datetime import datetime
-from typing import Dict, List, Tuple, Union
+from typing import List, Tuple, Union
+
+from .models import KeyAddResponse, KeyResponse
+
+base_indices = [
+    (0, 14),
+    (14, 34),
+    (34, 48),
+    (48, 76),
+]
+
+api_key_index = (76, 110)
 
 
-def print_header(add_key: bool = False) -> None:
-    header_items: List = [
-        ("IDENTIFIER", (0, 14)),
-        ("CAN CREATE TOKEN", (14, 34)),
-        ("CREATED BY", (34, 48)),
-        ("CREATION DATE", (48, 76)),
-    ]
-    if add_key:
-        header_items.append((("API KEY", (76, 110))))
-    header = "".join([pad(i, p) for i, p in header_items])
-    print(header)
+def print_output(*keys: Union[KeyResponse, KeyAddResponse]):
+    print_header_line(keys[0])
+    for key in keys:
+        print_line(key)
 
 
-def print_line(key: Dict, add_key: bool = False) -> None:
-    identifier: str = pad(key["identifier"], (0, 14))
-    can_create_token: str = pad(key["can_create_token"], (14, 34))
-    created_by: str = pad(key["created_by"], (34, 48))
-    c_time: str = datetime.fromisoformat(key["creation_date"]).ctime()
-    creation_date: str = pad(c_time, (48, 76))
-    line = "".join([identifier, can_create_token, created_by, creation_date])
-    if add_key:
-        api_key: str = pad(key["new_api_key"], (76, 110))
-        line + api_key
+def print_header_line(key: Union[KeyResponse, KeyAddResponse]) -> None:
+    line = construct_base_header_line()
+    if isinstance(key, KeyAddResponse):
+        line += pad("API KEY", api_key_index)
     print(line)
 
 
-def pad(value: Union[str, int, bool], position: Tuple[int, int]) -> str:
-    if not isinstance(value, str):
+def construct_base_header_line() -> str:
+    header_items = [
+        "IDENTIFIER",
+        "CAN CREATE TOKEN",
+        "CREATED BY",
+        "CREATION DATE",
+    ]
+    columns = [pad(value, index) for value, index in zip(header_items, base_indices)]
+    line = "".join(columns)
+    return line
+
+
+def print_line(key: Union[KeyResponse, KeyAddResponse]) -> None:
+    line = construct_base_line(key)
+    if isinstance(key, KeyAddResponse):
+        line += pad(key.new_api_key, api_key_index)
+    print(line)
+
+
+def construct_base_line(key: KeyResponse) -> str:
+    columns: List[str] = [
+        pad(value, index) for (_, value), index in zip(key, base_indices)
+    ]
+    line = "".join(columns)
+    return line
+
+
+def pad(value: Union[str, int, bool, datetime], position: Tuple[int, int]) -> str:
+    if isinstance(value, datetime):
+        value = value.ctime()
+    elif not isinstance(value, str):
         value = str(value)
     block_len: int = position[1] - position[0]
     spaces: int = block_len - len(value)
