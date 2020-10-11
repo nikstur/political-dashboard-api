741838abd28d0eb0cd225cef358c2c9728106cab
nikstur <nikstur@outlook.com>
2020-10-12 00:40:20 +0200

    Added and cleaned dependencies and tests

--- api/tests/test_dependencies.py
+++ api/tests/test_dependencies.py
@@ -58,26 +58,17 @@ async def test_time_query_exceptional_input(
     assert exception in str(excinfo)
 
 
-def test_construct_filter_from_date():
-    start_date = date(2020, 6, 7)
-    end_date = date(2020, 6, 8)
-    expected = {"date": {"$gte": datetime(2020, 6, 7), "$lt": datetime(2020, 6, 8)}}
-
-    time_filter = dependencies.construct_filter_from_date(start_date, end_date)
-    assert time_filter == expected
+# When connect() method has not been called on DataBase AttributeError should be raised
 
 
-def test_transform_date_to_datetime():
-    start_date = date(2020, 6, 7)
-    end_date = date(2020, 6, 8)
-    expected_start_datetime = datetime(2020, 6, 7, 0, 0)
-    expected_end_datetime = datetime(2020, 6, 8, 0, 0)
+def test_db_content_conn():
+    with pytest.raises(AttributeError):
+        dependencies.db_content_conn()
 
-    start_datetime, end_datetime = dependencies.transform_date_to_datetime(
-        start_date, end_date
-    )
-    assert start_datetime == expected_start_datetime
-    assert end_datetime == expected_end_datetime
+
+def test_db_admin_conn():
+    with pytest.raises(AttributeError):
+        dependencies.db_content_conn()
 
 
 @pytest.fixture
@@ -86,7 +77,12 @@ def db_admin():
         async def find(self, collection, identifier):
             data = {
                 1: {
-                    "hash": "$2b$12$7pmPKz6uqV5DIFR7b7R0IuWXND0WdPQDM/1neOf.oTJXclqPd.ReW"
+                    "hash": "$2b$12$7pmPKz6uqV5DIFR7b7R0IuWXND0WdPQDM/1neOf.oTJXclqPd.ReW",
+                    "can_create_token": True,
+                },
+                2: {
+                    "hash": "$2b$12$OzOQm7lmrvcYmAeaIjsuruJSa2N71I6rhK16mMqSW54tSDXPERrm.",
+                    "can_create_token": False,
                 },
             }
             return data.get(identifier)
@@ -102,23 +98,33 @@ async def test_api_key_query_acceptable_input(db_admin):
     assert api_key_input == api_key_output
 
 
-@pytest.mark.asyncio
-async def test_api_key_query_exceptional_input(db_admin):
-    api_key_input = "2-49e04d40093d55aa5cd2ba679a7e1486"
-    with pytest.raises(HTTPException) as excinfo:
-        await dependencies.api_key_query(api_key_input, db_admin)
-    assert "Not authenticated" in str(excinfo)
-
-
 @pytest.mark.parametrize(
-    "api_key,expected",
+    "api_key",
     [
-        ("1-49e04d40093d55aa5cd2ba679a7e1486", True),
-        ("2-49e04d40093d55aa5cd2ba679a7e1486", False),
-        ("1-49e04d40093d55aa6cd2ba679a7e1486", False),
+        "1-49e04d40093d55aa6cd2ba679a7e1486",  # Wrong hash
+        "2-49e04d40093d55aa5cd2ba679a7e1486",  # Correct hash with wrong identifier
+        "3-49e04d40093d55aa5cd2ba679a7e1486",  # Correct hash with unknown id
     ],
 )
 @pytest.mark.asyncio
-async def test_verify_key_hash(db_admin, api_key, expected):
-    is_verified = await dependencies.verify_key_hash(api_key, db_admin)
-    assert is_verified == expected
+async def test_api_key_query_exceptional_input(api_key, db_admin):
+    with pytest.raises(HTTPException) as excinfo:
+        await dependencies.api_key_query(api_key, db_admin)
+    assert "Not authenticated" in str(excinfo)
+
+
+@pytest.mark.asyncio
+async def test_admin_api_key_query_acceptable_input(db_admin):
+    api_key_input = "1-49e04d40093d55aa5cd2ba679a7e1486"
+    api_key_output = await dependencies.admin_api_key_query(api_key_input, db_admin)
+    assert api_key_input == api_key_output
+
+
+@pytest.mark.asyncio
+async def test_admin_api_key_query_exceptional_input(db_admin):
+    # Correct id and hash with wrong perms
+    api_key = "2-49fcde3548d7aaf096017a4b19877432"
+
+    with pytest.raises(HTTPException) as excinfo:
+        await dependencies.admin_api_key_query(api_key, db_admin)
+    assert "Insufficient rights to access this resource" in str(excinfo)
