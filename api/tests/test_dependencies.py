179a717b3ca44b9324a15b48c33f0ee90014a0f5
nikstur <nikstur@outlook.com>
2020-11-01 00:10:44 +0100

    Removed tests for security dependencies

--- api/tests/test_dependencies.py
+++ api/tests/test_dependencies.py
@@ -58,73 +58,9 @@ async def test_time_query_exceptional_input(
     assert exception in str(excinfo)
 
 
-# When connect() method has not been called on DataBase AttributeError should be raised
+# When connect() method has not been called on DataBase, AttributeError should be raised
 
 
-def test_db_content_conn():
+def test_db_conn():
     with pytest.raises(AttributeError):
-        dependencies.db_content_conn()
-
-
-def test_db_admin_conn():
-    with pytest.raises(AttributeError):
-        dependencies.db_content_conn()
-
-
-@pytest.fixture
-def db_admin():
-    class DBAdmin:
-        async def find(self, collection, identifier):
-            data = {
-                1: {
-                    "hash": "$2b$12$7pmPKz6uqV5DIFR7b7R0IuWXND0WdPQDM/1neOf.oTJXclqPd.ReW",
-                    "can_create_token": True,
-                },
-                2: {
-                    "hash": "$2b$12$OzOQm7lmrvcYmAeaIjsuruJSa2N71I6rhK16mMqSW54tSDXPERrm.",
-                    "can_create_token": False,
-                },
-            }
-            return data.get(identifier)
-
-    db_admin = DBAdmin()
-    return db_admin
-
-
-@pytest.mark.asyncio
-async def test_api_key_query_acceptable_input(db_admin):
-    api_key_input = "1-49e04d40093d55aa5cd2ba679a7e1486"
-    api_key_output = await dependencies.api_key_query(api_key_input, db_admin)
-    assert api_key_input == api_key_output
-
-
-@pytest.mark.parametrize(
-    "api_key",
-    [
-        "1-49e04d40093d55aa6cd2ba679a7e1486",  # Wrong hash
-        "2-49e04d40093d55aa5cd2ba679a7e1486",  # Correct hash with wrong identifier
-        "3-49e04d40093d55aa5cd2ba679a7e1486",  # Correct hash with unknown id
-    ],
-)
-@pytest.mark.asyncio
-async def test_api_key_query_exceptional_input(api_key, db_admin):
-    with pytest.raises(HTTPException) as excinfo:
-        await dependencies.api_key_query(api_key, db_admin)
-    assert "Not authenticated" in str(excinfo)
-
-
-@pytest.mark.asyncio
-async def test_admin_api_key_query_acceptable_input(db_admin):
-    api_key_input = "1-49e04d40093d55aa5cd2ba679a7e1486"
-    api_key_output = await dependencies.admin_api_key_query(api_key_input, db_admin)
-    assert api_key_input == api_key_output
-
-
-@pytest.mark.asyncio
-async def test_admin_api_key_query_exceptional_input(db_admin):
-    # Correct id and hash with wrong perms
-    api_key = "2-49fcde3548d7aaf096017a4b19877432"
-
-    with pytest.raises(HTTPException) as excinfo:
-        await dependencies.admin_api_key_query(api_key, db_admin)
-    assert "Insufficient rights to access this resource" in str(excinfo)
+        dependencies.db_conn()
